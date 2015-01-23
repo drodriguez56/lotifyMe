107319d23569cde79366a81adb235c1e2fdc3831
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-22 20:20:49 -0500

    Add User Migration

---
+++ db/migrate/20150123011620_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :phone
+      t.string :email
+      t.boolean :active
+      t.string :pasword_digest, null: false
+      t.timestamps
+    end
+  end
+end
