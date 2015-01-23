3fbcb2fedd370241596267ff15f78f021ca60463
carlschubert <carl.schubert@gmail.com>
2015-01-22 21:32:50 -0500

    add working seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Pick.create(number: '313556596306', game: 'powerball', draw_date: '01/20/15', user_id: 1)
+Pick.create(number: '313577796306', game: 'powerball', draw_date: '01/16/15', user_id: 1)
+Pick.create(number: '313556577706', game: 'powerball', draw_date: '01/13/15', user_id: 2)
+Pick.create(number: '377756596306', game: 'powerball', draw_date: '01/20/15', user_id: 2)
+Pick.create(number: '313557777306', game: 'powerball', draw_date: '01/09/15', user_id: 3)
+Pick.create(number: '377777596306', game: 'powerball', draw_date: '01/20/15', user_id: 4)
+
+
+User.create(username: 'steph', phone: Faker::PhoneNumber.phone_number, password: '123', email: 'steph@steph.com', active: true)
+User.create(username: 'daniel', phone: Faker::PhoneNumber.phone_number, password: '123', email: 'daniel@daniel.com', active: true)
+User.create(username: 'dan', phone: Faker::PhoneNumber.phone_number, password: '123', email: 'dan@dan.com', active: true)
+User.create(username: 'carl', phone: Faker::PhoneNumber.phone_number, password: '123', email: 'carl@carl.com', active: true)
+
+5.times do
+  User.create(username: Faker::Name.name, phone: Faker::Number.number(10), password: '123', email: Faker::Internet.email, active: false)
+end
