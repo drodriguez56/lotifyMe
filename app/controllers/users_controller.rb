be2d64db8042e3e0896354575a6043447b69fe1e
Daniel K. Chan <dkchan90@gmail.com>
2015-01-22 21:35:53 -0500

    update basic crud actions for users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_or_create_by(email: user_params[:email])
+    if @user.update(user_params)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:failure] = "Signup failed."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated."
+      redirect_to user_path(@user)
+    else
+      flash[:failure] = "Update unsuccessful."
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :phone, :password)
+    end
+
+end
