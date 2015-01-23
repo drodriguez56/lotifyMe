da5cdeffd3830c4fbd08937375cfe947f28f5b10
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-23 13:20:49 -0500

    Add user creation and pick creation logic to pick controller

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -2,18 +2,29 @@
   
    def new
      @pick = Pick.new
-     @user = User.new
    end
 
    def create
-     @user = User.find_or_create_by(params[:user])
-     @pick = Pick.new(params[:pick])
+     @user = User.find_by(email: params[:pick][:email])
+     if !@user
+        @user = User.create(email: params[:pick][:email])
+     end
+     @pick = Pick.new(pick_params)
      if @pick.save 
        @user.picks << @pick
-       render text: 'good'
+       if params[:commit]=="signup"
+         render new_user_path, locals: {email: params[:email]}
+       else
+         redirect_to root_path
+       end
      else
        render text: 'faild'
      end
    end
 
+   private
+
+    def pick_params
+      params.require(:pick).permit(:number, :game, :draw_date, :multiplier)
+    end
  end
