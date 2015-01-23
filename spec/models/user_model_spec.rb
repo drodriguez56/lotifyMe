15f287ebd7f56605966762f0e685ecc750ec9a1b
Stephanie Barreyro <ssbarreyro@ucdavis.edu>
2015-01-22 22:34:00 -0500

    User Model tests for email presence.

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 describe User do
   xit "should be associated with picks" do
     user = User.new(:picks => nil)
-    no_user_team.should_not be_valid
+    user.should_not be_valid
   end
 
-
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(User.all).not_to include user
+  end
 end
