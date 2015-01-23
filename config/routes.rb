3850f677257bd7ecc93875950eacfec4b9800f0d
Daniel K. Chan <dkchan90@gmail.com>
2015-01-23 11:48:24 -0500

    begin landing page working form

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get  'signup', to: 'users#new', as: 'signup'
   post 'signup', to: 'users#create'
+
+  get  'landing',  to: 'pick#landing'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
