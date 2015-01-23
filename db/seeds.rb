b68bea27afd26bdd2f64c99e6276fe47792f93ad
carlschubert <carl.schubert@gmail.com>
2015-01-23 12:27:20 -0500

    modify seed to match migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-Pick.create(number: '313556596306', game: 'powerball', draw_date: '01/20/15', user_id: 1)
-Pick.create(number: '313577796306', game: 'powerball', draw_date: '01/16/15', user_id: 1)
-Pick.create(number: '313556577706', game: 'powerball', draw_date: '01/13/15', user_id: 2)
-Pick.create(number: '377756596306', game: 'powerball', draw_date: '01/20/15', user_id: 2)
-Pick.create(number: '313557777306', game: 'powerball', draw_date: '01/09/15', user_id: 3)
-Pick.create(number: '377777596306', game: 'powerball', draw_date: '01/20/15', user_id: 4)
+Pick.create(number: '313556596306', game: 'powerball', draw_date: Faker::Time.between(30.days.ago, Time.now), user_id: 1)
+Pick.create(number: '313577796306', game: 'powerball', draw_date: Faker::Time.between(30.days.ago, Time.now), user_id: 1)
+Pick.create(number: '313556577706', game: 'powerball', draw_date: Faker::Time.between(30.days.ago, Time.now), user_id: 2)
+Pick.create(number: '377756596306', game: 'powerball', draw_date: Faker::Time.between(30.days.ago, Time.now), user_id: 2)
+Pick.create(number: '313557777306', game: 'powerball', draw_date: Faker::Time.between(30.days.ago, Time.now), user_id: 3)
+Pick.create(number: '377777596306', game: 'powerball', draw_date: Faker::Time.between(30.days.ago, Time.now), user_id: 4)
 
 
 User.create(username: 'steph', phone: Faker::PhoneNumber.phone_number, password: '123', email: 'steph@steph.com', active: true)
