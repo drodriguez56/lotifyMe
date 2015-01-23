5b6f25b8f9f98bb79c8f8a0109ee3f9c9153d38a
Stephanie Barreyro <ssbarreyro@ucdavis.edu>
2015-01-22 21:12:49 -0500

    Created pending Pick Model tests.

---
+++ spec/models/pick_model_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Pick do
+   it "is valid with numbers, game, draw_date"
+   it "is invalid without numbers"
+   it "is invalid without game"
+   it "is invalid with draw_date"
+   it "does not allow duplicate emails"
+end
