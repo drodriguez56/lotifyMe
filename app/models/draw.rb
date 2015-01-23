15f5b587856f08281041c8acc1a494b45f179735
carlschubert <carl.schubert@gmail.com>
2015-01-23 12:27:55 -0500

    add method to get powerball data

--- app/models/draw.rb
+++ app/models/draw.rb
@@ -1,5 +1,20 @@
+require 'net/http'
+
 class Draw < ActiveRecord::Base
   validates :number, presence: true
   validates :game, presence: true
   validates :draw_date, presence: true
+
+  def get_data_powerball
+    uri = URI("https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD")
+    response = Net::HTTP.get_response(uri)
+    if response.code == "200"
+      result = JSON.parse(response.body)
+      result["data"].each do |drawing|
+        draw = Draw.new(number: drawing[9], game: 'powerball', draw_date: drawing[8], multiplier: drawing[10])
+        draw.save
+      end
+    else
+    end
+  end
 end
