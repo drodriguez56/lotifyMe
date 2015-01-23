1c1d3c551b39ac4f563584bae2e7e088974ed80d
Stephanie Barreyro <ssbarreyro@ucdavis.edu>
2015-01-22 21:18:33 -0500

    Pick Model specs for valid numbers, game, and draw_date.

--- spec/models/pick_model_spec.rb
+++ spec/models/pick_model_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 describe Pick do
-   it "is valid with numbers, game, draw_date"
+   it "is valid with numbers, game, draw_date" do
+    pick = Pick.new(
+      numbers:"313577796306",
+      game: "powerball",
+      draw_date: "01/13/15")
+    expect(pick).to be_truthy
+   end
    it "is invalid without numbers"
    it "is invalid without game"
    it "is invalid with draw_date"
