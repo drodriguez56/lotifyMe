9682ec981f7f87facda06508cd1e8fae09c3b683
Stephanie Barreyro <ssbarreyro@ucdavis.edu>
2015-01-22 21:08:14 -0500

    Draw Model specs validates if draw_date is nil.

--- spec/models/draw_model_spec.rb
+++ spec/models/draw_model_spec.rb
@@ -8,10 +8,13 @@ describe Draw do
     expect(draw).to be_truthy
   end
 
-   it "is invalid without a number" do
+  it "is invalid without a number" do
     draw = Draw.new(numbers: nil)
     expect(Draw.all).not_to include draw
   end
 
-
+  it "is invalid without a draw_date" do
+    draw = Draw.new(draw_date: nil)
+    expect(Draw.all).not_to include draw
+  end
 end
