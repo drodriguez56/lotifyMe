b03aefdc11863402169a1dc3c43e3027cd74efa9
Stephanie Barreyro <ssbarreyro@ucdavis.edu>
2015-01-22 20:51:44 -0500

    Draw Model test for nil numbers.

--- spec/models/draw_model_spec.rb
+++ spec/models/draw_model_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 describe Draw do
   it "is valid with a number and draw_date" do
     draw = Draw.new(
-      number: "11,23,33,04,20,24",
+      numbers: "11,23,33,04,20,24",
       draw_date: "2015-01-30")
     expect(draw).to be_truthy
   end
+
+   it "is invalid without a number" do
+    draw = Draw.new(numbers: nil)
+    expect(Draw.all).not_to include draw
+  end
+
+
 end
