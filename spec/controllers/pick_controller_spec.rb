727c45bdd21d2cbba3123b5e4924c53f1f52b2a6
Stephanie Barreyro <ssbarreyro@ucdavis.edu>
2015-01-23 18:37:37 -0500

    Created a fake Pick under Pick Controller Specs so it redirects to edit_user_path after its created.

--- spec/controllers/pick_controller_spec.rb
+++ spec/controllers/pick_controller_spec.rb
@@ -14,14 +14,26 @@ describe PicksController do
   end
 
   describe "POST #create" do
-    it "redirects to root_path if pick is created" do
-      post :create, pick: attributes_for(:pick)
-      expect(response).to redirect_to edit_user_path
+    let(:pick_params) do {
+      pick: {
+        email: "-666@example.com",
+        number: '374321106',
+        game: "snapple",
+        draw_date: Faker::Time.between(7.days.ago, Time.now),
+        multiplier: 2
+      },
+      commit: "signup"
+    }
+    end
+
+    it "redirects to edit_user_path if pick is created" do
+      post :create, pick_params
+      expect(response).to redirect_to edit_user_path(assigns[:user].id)
     end
 
     it "redirects to signup form if signup fails" do
       post :create, pick: attributes_for(:failed_pick)
-      expect(response).to redirect_to new_pick_path
+      expect(response).to redirect_to root_path
     end
   end
 
