59cd637c99812a66772a590f76f44d25af94b4b5
carlschubert <carl.schubert@gmail.com>
2015-01-24 17:42:07 -0500

    refactor get data to accept multiple JSONs

--- app/models/draw.rb
+++ app/models/draw.rb
@@ -7,16 +7,41 @@ class Draw < ActiveRecord::Base
 
   has_many :picks
 
-  def get_data_powerball
-    uri = URI("https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD")
+
+
+  def get_data(type)
+    uri = URI(type['url'])
     response = Net::HTTP.get_response(uri)
     if response.code == "200"
       result = JSON.parse(response.body)
-      result["data"].each do |drawing|
-        draw = Draw.new(number: drawing[9], game: 2, draw_date: drawing[8], multiplier: drawing[10])
-        draw.save
-      end
-    else
+      result["data"].each(&type['proc'])
     end
+  end
+
+  def powerball
+    return { "url" => 'https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD',
+           "proc" => Proc.new { |drawing|
+        draw = Draw.new(number: drawing[9], game: 'powerball', draw_date: drawing[8], multiplier: drawing[10])
+        draw.save
+      }
+    }
+  end
+
+  def mega_millions
+    return { "url" => "https://data.ny.gov/api/views/5xaw-6ayf/rows.json?accessType=DOWNLOAD",
+           "proc" => Proc.new { |drawing|
+        draw = Draw.new(number: (drawing[9] + ' ' + drawing[10]), game: 'mega_millions', draw_date: drawing[8], multiplier: drawing[11])
+        draw.save
+      }
+    }
+  end
+
+  def nylotto
+    return { "url" => "https://data.ny.gov/api/views/6nbc-h7bj/rows.json?accessType=DOWNLOAD",
+           "proc" => Proc.new { |drawing|
+        draw = Draw.new(number: (drawing[9] + ' ' + drawing[10]), game: 'mega_millions', draw_date: drawing[8], multiplier: drawing[11])
+        draw.save
+      }
+    }
   end
 end
