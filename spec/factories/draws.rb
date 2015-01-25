d11e65cf1822e545f85a2da4511313cfb028eb30
carlschubert <carl.schubert@gmail.com>
2015-01-25 14:58:18 -0500

    add draw factories for spec

--- spec/factories/draws.rb
+++ spec/factories/draws.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
-  factory :draw, class: Draw do
+  factory :draw_powerball, class: Draw do
     draw_date { '24-01-2015' }
     game { "powerball" }
-    multiplier { rand(1..5) }
+    multiplier { 2 }
+    number { '11 12 13 14 15 16' }
+  end
+
+  factory :draw_mega_millions, class: Draw do
+    draw_date { '21-01-2015' }
+    game { "mega_millions" }
+    multiplier { 2 }
     number { '11 12 13 14 15 16' }
   end
 end
