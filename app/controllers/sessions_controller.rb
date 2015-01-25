309d8ebc6f4c0ca8a339878c66945d6856a033ff
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-25 18:07:28 -0500

    Add API handling to session login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,22 @@ class SessionsController < ApplicationController
     user = Session.login_by_username_or_email(params[:user_input])
 
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.username}!"
-      redirect_to root_path
+      respond_to do |format|
+        format.json { render json: 'logged in ok', status: 200}
+        format.html {
+          session[:user_id] = user.id
+          flash[:success] = "Welcome, #{user.username}!"
+          redirect_to root_path
+        }
+       end
     else
-      flash[:failure] = "Login failed."
-      render :login
+      respond_to do |format|
+        format.json { render json: 'login failed', status: 400}
+        format.html {
+          flash[:failure] = "Login failed."
+          render :login
+        }
+       end
     end
   end
 
