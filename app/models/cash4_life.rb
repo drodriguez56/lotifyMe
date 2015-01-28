0628c85e3202bed64c58754b737bb4d04e27d955
carlschubert <carl.schubert@gmail.com>
2015-01-28 17:15:13 -0500

    refactor cash4life models out of picks

--- app/models/cash4_life.rb
+++ app/models/cash4_life.rb
@@ -10,4 +10,50 @@ class Cash4Life < Draw
       end
     end
   end
+
+  def find_score(pick)
+    pickarr = pick.number.split(' ')
+    drawarr = self.number.split(' ')
+    power_pick = pickarr.pop
+    power_draw = drawarr.pop
+    match = pickarr & drawarr
+      if power_pick == power_draw
+        score = match.length.to_s + 'P'
+      else
+        score = match.length.to_s
+      end
+      self.translate_score(score)
+  end
+
+  def translate_score(score)
+    case score
+      when '1P'
+        winnings = 2
+        return "You won $#{winnings}"
+      when '2'
+        winnings = 4
+        return "You won $#{winnings}"
+      when '2P'
+        winnings = 10
+        return "You won $#{winnings}"
+      when '3'
+        winnings = 25
+        return "You won $#{winnings}"
+      when '3P'
+        winnings = 100
+        return "You won $#{winnings}"
+      when '4'
+        winnings = 500
+        return "You won $#{winnings}"
+      when '4P'
+        winnings = 2500
+        return "You won $#{winnings}"
+      when '5'
+        return 'You won $1,000 a week for Life}'
+      when '5P'
+        return 'You won $1,000 a day for Life'
+      else
+        return 'You did not win'
+    end
+  end
 end
