f3c89aa41e639241289bf0a57ad2c28995722356
carlschubert <carl.schubert@gmail.com>
2015-01-28 17:16:17 -0500

    refactor nylotto  methods  out of picks

--- app/models/ny_lotto.rb
+++ app/models/ny_lotto.rb
@@ -10,4 +10,45 @@ class NyLotto < Draw
       end
     end
   end
+
+  def find_score(pick)
+    pickarr = pick.number.split(' ')
+    drawarr = self.number.split(' ')
+    power_pick = pickarr.pop
+    power_draw = drawarr.pop
+    match = pickarr & drawarr
+      if power_pick == power_draw
+        score = match.length.to_s + 'P'
+      else
+        score = match.length.to_s
+      end
+      self.translate_score(score)
+  end
+
+  def translate_score(score)
+    case score
+      when '3'
+       winnings = '5th place'
+       return "You won #{winnings}"
+      when '3P'
+       winnings = '5th place'
+       return "You won #{winnings}"
+      when '4'
+       winnings = '4th place'
+       return "You won #{winnings}"
+      when '4P'
+       winnings = '4th place'
+       return "You won #{winnings}"
+      when '5'
+       winnings = '3rd place'
+       return "You won #{winnings}"
+      when '5P'
+       winnings = '2nd place'
+       return "You won #{winnings}"
+      when '6P'
+       return 'JACKPOT!!'
+      else
+       return 'You did not win'
+   end
+ end
 end
