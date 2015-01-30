7b858b4735137fe483c0dde3defa4ebcceebb5c2
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-29 19:11:01 -0500

    2 lotos to demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ p = Powerball.new
 p.get_data!
 m = MegaMillion.new
 m.get_data!
-n = NyLotto.new
-n.get_data!
-c = Cash4Life.new
-c.get_data!
-t = Take5.new
-t.get_data!
-p = Pick10.new
-p.get_data!
+# n = NyLotto.new
+# n.get_data!
+# c = Cash4Life.new
+# c.get_data!
+# t = Take5.new
+# t.get_data!
+# p = Pick10.new
+# p.get_data!
