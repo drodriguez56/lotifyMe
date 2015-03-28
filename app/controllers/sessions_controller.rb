b29c59aad2f58a80128147506577f3e2022d6007
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-03-28 14:21:51 -0400

    Change path after user creation to user path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
         format.html {
           session[:user_id] = user.id
           flash[:success] = "Welcome, #{user.username}!"
-          redirect_to root_path
+          redirect_to user_path(user)
         }
        end
     else
